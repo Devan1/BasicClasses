92527ad5390b468615c2a6efbd150993170bc0a2
Devan <deshlock010101@hotmai.com>
2022-02-21 09:45:15 -0600

    Add project files.

---
+++ BasicClasses/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses
+{
+    public class Car
+    {
+        //Constructors
+        public Car()
+        {
+
+        }
+        //Fields
+        //Properties
+        //Methods
+        public string make { get; set; } //get = read set = write
+        public string model { get; set; }
+        public int year { get; set; }
+    }
+}
---
+++ BasicClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.make = "Toyota";
+            myCar.model = "Tundra";
+            myCar.year = 2006;
+            Console.WriteLine(myCar.year);
+            Console.WriteLine(myCar.make);
+            Console.WriteLine(myCar.model);           
+        }
+    }
+}
